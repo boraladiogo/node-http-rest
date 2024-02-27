5af3e65ae6a6cd2b38e9a3ad84c94252d4dfaf5f
Diogo Martins <boraladiogo@gmail.com>
2024-02-27 11:56:36 -0300

    Create an example on how to add a route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
-const server = http.createServer((request, res) => {
+const server = http.createServer((request, response) => {
     console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+
+    if (request.method === 'GET' && request.url === '/') {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end('<h1>Home</h1>')
+    }
 });
 
 server.listen(3000, () => console.log('Server started at http://localhost:3000'));

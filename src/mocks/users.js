4b6f01e2d1ab663fa5dac7356ea95481638689ec
Diogo Martins <boraladiogo@gmail.com>
2024-02-27 13:58:59 -0300

    Create  users mock to demo route responses

---
+++ src/mocks/users.js
@@ -0,0 +1,25 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Diogo',
+        age: 34
+    },
+
+    {
+        id: 2,
+        name: 'Thais',
+        age: 31
+    },
+
+    {
+        id: 3,
+        name: 'Joaquim',
+        age: 9
+    },
+
+    {
+        id: 4,
+        name: 'Cecília',
+        age: 0
+    }
+];

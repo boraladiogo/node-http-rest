637045b4684060a210196efce6078926fce57cca
Diogo Martins <boraladiogo@gmail.com>
2024-02-28 18:37:29 -0300

    Add new GET route 'users/:id' to retrieve user by ID

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,13 @@ const UserController = require("./controllers/UserController");
 
 module.exports = [
     {
-      endpoint: '/users',
-      method: 'GET',
-      handler: UserController.list  
+        endpoint: '/users',
+        method: 'GET',
+        handler: UserController.list  
+    },
+    {
+        endpoint: '/userd/:id',
+        method: 'GET',
+        handler: UserController.getById
     }
 ];
